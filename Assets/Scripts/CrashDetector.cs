e09868270a61f707363e526a1c2d0139e546d99d
Enes_Yıldız <enesyildizmr@gmail..com>
2023-03-20 22:37:50 +0300

    Added colliders for post and crash

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Ground")
+            Debug.Log("You crashed");
+    }
+}
